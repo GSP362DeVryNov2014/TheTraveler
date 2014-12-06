53edb93b7836d06fff7f005b263f38d72fd20671
jajav33 <bocon5bocon@yahoo.com>
2014-12-06 13:58:33 -0800

    merging item fix from Damien
    merging Damiens fix for items in GamePlayStateMachine.cs

---
+++ Assets/Scripts/GUIScripts/MainBars/GUIBottomBar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace GSP.JAVIERGUI
+{
+
+	public class GUIBottomBar : MonoBehaviour 
+	{
+
+		// Use this for initialization
+		void Start () {
+		
+		}
+
+
+		void OnGUI()
+		{
+
+
+		}	//end OnGUI()
+
+	}	//end public class GUIBottomBar : MonoBehaviour
+
+}	//end namespace GSP.JAVIERGUI
