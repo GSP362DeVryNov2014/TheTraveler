455f7bda6439f4d8dda22aa88f1e84a6dee98c02
Damien <hadesrealm@gmail.com>
2014-12-07 11:15:26 -0800

    MenuData Skeleton Added.
    The skeleton containing the middle man for passing information from the
    menu scene. Currently only used for the number of players.

---
+++ Assets/Scripts/MenuData.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace GSP
+{
+	public class MenuData : MonoBehaviour
+	{
+		// Holds the number of players for the game.
+		int m_numberPlayers;
+		
+		public int NumberPlayers
+		{
+			get { return m_numberPlayers; }
+		} // end NumberPlayers property
+		
+		// Use this for initialisation.
+		void Start()
+		{
+			// Initialise the variables to zero.
+			m_numberPlayers = 0;
+		} // end Start function
+	} // end MenuData class
+}
